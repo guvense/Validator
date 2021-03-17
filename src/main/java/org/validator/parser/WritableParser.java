ed1533a5d488401bdaee9c3f4300f79333efe83b
guvense <guven.seckin.4@gmail.com>
2021-03-18 00:16:13 +0300

    Add somessssss

--- src/main/java/org/validator/parser/WritableParser.java
+++ src/main/java/org/validator/parser/WritableParser.java
@@ -90,6 +90,7 @@ public class WritableParser {
             return StringUtils.uncapitalize(argumentTypeCore);
         }
 
+
         private void fillImportPaths (ArrayList < String > imports, ValidatorMethod validatorMethod){
             String argumentPath = validatorMethod.getArgumentType();
             imports.add(argumentPath);
