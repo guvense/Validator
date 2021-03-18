4298a35a903bea4d909f70d1f847259e6526e14c
guvense <guven.seckin.4@gmail.com>
2021-03-18 12:31:18 +0300

    Add somessssss3

--- src/main/java/org/validator/parser/AnnotationFieldParser.java
+++ src/main/java/org/validator/parser/AnnotationFieldParser.java
@@ -4,6 +4,7 @@ package org.validator.parser;
 import com.sun.tools.javac.code.Attribute;
 import com.sun.tools.javac.code.Symbol;
 import com.sun.tools.javac.util.Pair;
+
 import org.validator.generator.constant.ConditionRule;
 import org.validator.generator.constant.Pattern;
 import org.validator.parser.model.ValidatorDetail;
